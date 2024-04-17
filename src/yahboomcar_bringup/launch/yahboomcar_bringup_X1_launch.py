4631b51b35082a76e838757e88a2c0f647e9f9c8
Davin-Liang <1528994924@qq.com>
2024-04-17 17:46:40 +0800

    modify imu param file

--- src/yahboomcar_bringup/launch/yahboomcar_bringup_X1_launch.py
+++ src/yahboomcar_bringup/launch/yahboomcar_bringup_X1_launch.py
@@ -80,7 +80,7 @@ def generate_launch_description():
     imu_filter_node = Node(
         package='imu_filter_madgwick',
         executable='imu_filter_madgwick_node',
-        parameters=[imu_filter_config]
+        parameters=['~/farming_robot/farming_ws/src/yahboomcar_bringup/param/imu_filter_param.yaml']
     )
 
     ekf_node = IncludeLaunchDescription(

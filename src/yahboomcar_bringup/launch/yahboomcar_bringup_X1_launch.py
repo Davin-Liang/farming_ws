55cd85bdf895247c82e5bf56caf831f407967347
Davin-Liang <1528994924@qq.com>
2024-04-27 17:51:04 +0800

    fafasd

--- src/yahboomcar_bringup/launch/yahboomcar_bringup_X1_launch.py
+++ src/yahboomcar_bringup/launch/yahboomcar_bringup_X1_launch.py
@@ -26,7 +26,7 @@ def generate_launch_description():
                                       description='Absolute path to robot urdf file')
     rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
                                      description='Absolute path to rviz config file')
-    pub_odom_tf_arg = DeclareLaunchArgument('pub_odom_tf', default_value='true',
+    pub_odom_tf_arg = DeclareLaunchArgument('pub_odom_tf', default_value='false',
                                             description='Whether to publish the tf from the original odom to the base_footprint')
 
     robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
@@ -118,8 +118,8 @@ def generate_launch_description():
         
         driver_node,
         base_node,
-        # imu_filter_node,
-        # ekf_node,
+        imu_filter_node,
+        ekf_node,
         yahboom_joy_node,
         base_footprint_tf,
         imu_tf

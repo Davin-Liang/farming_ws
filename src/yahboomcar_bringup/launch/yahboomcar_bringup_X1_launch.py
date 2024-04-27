7fc9dad23219a530b98ad79460b4ae4cc15a50f2
Davin-Liang <1528994924@qq.com>
2024-04-27 20:09:52 +0800

    fdfasd

--- src/yahboomcar_bringup/launch/yahboomcar_bringup_X1_launch.py
+++ src/yahboomcar_bringup/launch/yahboomcar_bringup_X1_launch.py
@@ -92,7 +92,7 @@ def generate_launch_description():
     imu_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('imu_complementary_filter'), 'launch'),
-            '/imu_filter.launch.py'])
+            '/complementary_filter.launch.py'])
     )
 
     yahboom_joy_node = Node(

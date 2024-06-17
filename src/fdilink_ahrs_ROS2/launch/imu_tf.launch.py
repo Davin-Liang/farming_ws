45428f434e042eb360b6d5eae6b5c4f872db9d16
Davin-Liang <1528994924@qq.com>
2024-06-17 20:22:21 +0800

    改正 IMU 功能包中的 imu_tf.launch.py 中存在的错误，同时修改了 wheeltec_udev.sh

--- src/fdilink_ahrs_ROS2/launch/imu_tf.launch.py
+++ src/fdilink_ahrs_ROS2/launch/imu_tf.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     imu_tf=Node(
         package="fdilink_ahrs",
-        #node_executable="imu_tf_node",
+        executable="imu_tf_node",
         parameters=[{'imu_topic':'/imu',
         'world_frame_id':'/world',
         'imu_frame_id':'/gyro_link',
--- src/fdilink_ahrs_ROS2/wheeltec_udev.sh
+++ src/fdilink_ahrs_ROS2/wheeltec_udev.sh
@@ -1,11 +1,11 @@
 #CP2102 串口号0003 设置别名为fdilink_ahrs
 echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60",ATTRS{serial}=="0003", MODE:="0777", GROUP:="dialout", SYMLINK+="fdilink_ahrs"' >/etc/udev/rules.d/wheeltec_fdi_imu_gnss.rules
 #CH9102，同时系统安装了对应驱动 串口号0003 设置别名为fdilink_ahrs
-echo  'KERNEL=="ttyCH343USB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="55d4",ATTRS{serial}=="0003", MODE:="0777", GROUP:="dialout", SYMLINK+="fdilink_ahrs"' >/etc/udev/rules.d/wheeltec_fdi_imu_gnss2.rules
+#echo  'KERNEL=="ttyCH343USB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="55d4",ATTRS{serial}=="0003", MODE:="0777", GROUP:="dialout", SYMLINK+="fdilink_ahrs"' >/etc/udev/rules.d/wheeltec_fdi_imu_gnss2.rules
 #CH9102，同时系统没有安装对应驱动 串口号0003 设置别名为fdilink_ahrs
-echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="55d4",ATTRS{serial}=="0003", MODE:="0777", GROUP:="dialout", SYMLINK+="fdilink_ahrs"' >/etc/udev/rules.d/wheeltec_fdi_imu_gnss3.rules
+#echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="55d4",ATTRS{serial}=="0003", MODE:="0777", GROUP:="dialout", SYMLINK+="fdilink_ahrs"' >/etc/udev/rules.d/wheeltec_fdi_imu_gnss3.rules
 #CH340，直接设置别名为fdilink_ahrs
-echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE:="0777", GROUP:="dialout", SYMLINK+="fdilink_ahrs"' >/etc/udev/rules.d/wheeltec_fdcontroller_340.rules
+#echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", MODE:="0777", GROUP:="dialout", SYMLINK+="fdilink_ahrs"' >/etc/udev/rules.d/wheeltec_fdcontroller_340.rules
 
 service udev reload
 sleep 2

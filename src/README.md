23951ac628aabf9678881566098f776651bff2f0
Davin-Liang <1528994924@qq.com>
2024-06-23 11:56:06 +0800

    完善了 upupup.launch，并创建了 README.md

---
+++ src/README.md
@@ -0,0 +1,23 @@
+# 下地干活
+## 1、重要事项
+1. 车体不得一丝磕碰，预防影响 IMU 精度
+2. 不启动 IMU 读取程序时，需拔掉 IMU 的 USB 读取接口
+3. 不在运行程序的 X3 派和 IMU 需用小风扇对着吹
+4. X3 派初开机时，先录音，激活音频板
+
+## 2、操作流程
+1. 准备好三个终端，一个用于启动 launch，一个用于启动 python 文件，一个用于测试音频板
+2. 比赛前测试音频板是否可以播放音频
+'''
+sudo tinyplay ./voice/up.wav -D 0 -d 0
+'''
+3. 用各种工具不断调整车体的初始位置，这个期间电脑掌控者不断测试音频版是否能播放音频
+4. 在确保车体的初始位置正确和音频板能播放音频后，准备启动程序
+5. 启动 upupup.launch.py
+'''
+ros2 launch yahboomcar_bringup upupup.launch
+'''
+6. 在确保 launch 正常运行后，运行 farming_visioner.py
+'''
+python3 farming_visioner.py
+'''
--- src/yahboomcar_bringup/launch/upupup.launch.py
+++ src/yahboomcar_bringup/launch/upupup.launch.py
@@ -18,7 +18,6 @@ print("---------------------robot_type = x1---------------------")
 def generate_launch_description():
     urdf_tutorial_path = get_package_share_path('yahboomcar_description')
     default_model_path = urdf_tutorial_path / 'urdf/yahboomcar_X3.urdf'
-    default_rviz_config_path = urdf_tutorial_path / 'rviz/yahboomcar.rviz'
 
     gui_arg = DeclareLaunchArgument(name='gui', 
                                     default_value='false', 
@@ -27,9 +26,6 @@ def generate_launch_description():
     model_arg = DeclareLaunchArgument(name='model', 
                                       default_value=str(default_model_path),
                                       description='Absolute path to robot urdf file')
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', 
-                                     default_value=str(default_rviz_config_path),
-                                     description='Absolute path to rviz config file')
     pub_odom_tf_arg = DeclareLaunchArgument('pub_odom_tf', 
                                             default_value='false',
                                             description='Whether to publish the tf from the original odom to the base_footprint')
@@ -130,7 +126,6 @@ def generate_launch_description():
     return LaunchDescription([
         gui_arg,
         model_arg,
-        rviz_arg,
         pub_odom_tf_arg,
         fdilink_ahrs_node,
         
